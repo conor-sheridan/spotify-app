a119065b360d7534ab547c5db136a80cfd6919b9
Conor Sheridan <conorksheridan@gmail.com>
2021-03-09 10:27:09 -0500

    Login page styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '../material.module';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +17,8 @@ import { AppComponent } from './app.component';
     MaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [
+    AppComponent,
+    ToolbarComponent]
 })
 export class AppModule { }
